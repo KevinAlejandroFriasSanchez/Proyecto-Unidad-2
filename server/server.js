308f78a2ef028bb3c839619ed2e34e9254ad55c5
kevin.frias <alejandro.atem@gmail.com>
2020-11-26 17:15:53 -0600

    Creacion de una carpeta de rutas

--- server/server.js
+++ server/server.js
@@ -8,82 +8,24 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
  
-    app.get('/', function (req, res) {
-    res.send('<h1>Bienvenido a  mi servidor REST</h1>');
-    });
- 
-    app.get('/usuario', function (req, res) {
-    res.json({
-        ok: 200,
-        mensaje: 'Usuarios consultados con exito'
-    })
-    });
+app.get('/', function(req, res) {
+    res.send('<h1>Bienvenido a mi servidor REST</h1>');
+});
+     
 
-    app.post('/usuario',function (req, res){
-    let nombre = req.body.nombre;
-    let body = req.body;
 
-        if(nombre === undefined){
-            res.status(400).json({
-                ok: 400,
-                mensaje: 'Favor de enviar el valor del nombre',
-            });
-        }else{
-            res.json ({
-                ok: 200,
-                mensaje: 'Usuario insertado con exito',
-                body: body
-            })
-        }
-            res.json ({
-                ok: 200,
-                mensaje: 'Usuario insertado con exito',
-                body: body
-            })
-        });
+app.use(require('./routes/usuarios'));
 
-    app.put('/usuario/:id/:nombre', function(req, res){
-        let id = req.params.id;
-        let nombre = req.params.nombre;
-
-        res.json({
-            ok: 200,
-            mensaje: 'Usuario actualizado con exito',
-            id:id,
-            nombre: nombre
-        });
-    });
-
-    app.delete('/usuario/:id', function(req, res){
-        let id = req.params.id;
-
-        res.json ({
-            ok: 200,
-            mensaje: 'Usuario eliminado con exito',
-            id: id
-        });
-    });
-
-  app.get('/saludo', function (req, res) {
-    res.json ({
-        ok: "200",
-        mensaje: 'bienvenido kevin',
-        });
-  });
-
- 
-  mongoose.connect('mongodb://localhost:27017/cafeteria' , {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-        
-  },(err,res) => {
-  if (err) throw err;
-  
-  console.log('Base de datos ONLINE');
-  });
+mongoose.connect('mongodb://localhost:27017/cafeteria', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+});
 
 app.listen(process.env.PORT, () => {
-  console.log ('el servidor esta en linea por el puerto', process.env.PORT)  
+    console.log('El servidor esta en linea por el puerto ', process.env.PORT);
 });
