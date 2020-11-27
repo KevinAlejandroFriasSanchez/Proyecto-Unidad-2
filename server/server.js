0ddc5bccb5eefb8dd2aa04632d549ae80f280d5b
kevin.frias <alejandro.atem@gmail.com>
2020-11-27 15:11:14 -0600

    Servido listo para tarea de heroku

--- server/server.js
+++ server/server.js
@@ -16,15 +16,16 @@ app.get('/', function(req, res) {
 
 app.use(require('./routes/usuarios'));
 
-mongoose.connect('mongodb://localhost:27017/cafeteria', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-}, (err, res) => {
-    if (err) throw err;
-    console.log('Base de datos ONLINE');
-});
+    //mongoose.connect('mongodb://localhost:27017/cafeteria', {
+        //useNewUrlParser: true,
+        //useCreateIndex: true,
+        //useUnifiedTopology: true,
+        //useFindAndModify: false
+    //}, (err, res) => {
+      //  if (err) throw err;
+        //console.log('Base de datos ONLINE');
+    //});
+
 
 app.listen(process.env.PORT, () => {
     console.log('El servidor esta en linea por el puerto ', process.env.PORT);
