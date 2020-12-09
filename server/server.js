d5e91dc936edb8b2ccbcb70f6b5c5d8167338e65
kevin.frias <alejandro.atem@gmail.com>
2020-12-09 12:44:57 -0600

    Se introdujo codigo para conectar con angular

--- server/server.js
+++ server/server.js
@@ -7,7 +7,15 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
- 
+
+app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+    next();
+});
 app.get('/', function(req, res) {
     res.send('<h1>Bienvenido a mi servidor REST</h1>');
 });
