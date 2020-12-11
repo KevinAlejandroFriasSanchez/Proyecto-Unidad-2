41fc893bc7fd73c127f0c71f2e0a476abaa6a7f0
kevin.frias <alejandro.atem@gmail.com>
2020-12-11 03:48:58 -0600

    Se instalo el paquete cors para la conexion con angular

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
 require('./config/config');
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require ('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
@@ -19,6 +17,10 @@ app.use((req, res, next) => {
 app.get('/', function(req, res) {
     res.send('<h1>Bienvenido a mi servidor REST</h1>');
 });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use(require('./routes/login'))
 app.use(require('./routes/producto'));
